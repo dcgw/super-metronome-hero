94112ed5492475e20abf940d6fc3b90afe82c8cc
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-11-16 18:45:18 +0000

    chore: add type definitions for assets loaded by webpack

---
+++ types/loaders.d.ts
@@ -0,0 +1,54 @@
+declare module "*.jpeg" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.jpg" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.gif" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.mp3" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.mp4" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.oga" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.ogg" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.ogv" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.png" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.webm" {
+    const url: string;
+    export default url;
+}
+
+declare module "*.webp" {
+    const url: string;
+    export default url;
+}
