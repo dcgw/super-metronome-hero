653a6cab46c1e5c5d93a4b29db68e2dd457380fe
Ian Johnson <github@humblecode.ninja>
2020-01-26 20:09:46 +0000

    feat(performance.ts): add two test Labels for tempo and bpm
    These don't work yet and aren't hooked up dynamically but they at least render

--- performance/performance.ts
+++ performance/performance.ts
@@ -1,39 +1,53 @@
-import {Actor, Engine, Input, Scene, Sound, Vector} from "excalibur";
+import {
+  Actor,
+  Color,
+  Engine,
+  Input,
+  Label,
+  Scene,
+  Sound,
+  Vector
+} from "excalibur";
 import Game from "../game";
 import resources from "../resources";
 import Timer from "../timer";
 import Arm from "./arm";
 
-const introDuration = 30 / 60 * 1000;
-const outroDuration = 50 / 60 * 1000;
+const introDuration = (30 / 60) * 1000;
+const outroDuration = (50 / 60) * 1000;
 // const baseScore = 7;
 const maxMissedBeats = 4;
 
 enum State {
-    intro = "intro",
-    countIn = "count-in",
-    play = "play",
-    outro = "outro",
-    result = "result",
-    done = "done",
-    return = "return"
+  intro = "intro",
+  countIn = "count-in",
+  play = "play",
+  outro = "outro",
+  result = "result",
+  done = "done",
+  return = "return"
 }
 
 export class Performance extends Scene {
     private timer: Timer;
-    // private score = 0;
-    // private multiplier = 0;
+  // private score = 0;
+  // private multiplier = 0;
     private missedBeats = 0;
     private introBeat = 0;
     private beats = 0;
     private state = State.intro;
     private time = 0;
+    private tempo = "";
+    private tempoText: Label | null = null;
+    private bpmText: Label | null = null;
 
     private readonly arm = new Arm();
 
     private readonly countdown: ReadonlyArray<Sound> = [
-        resources.performanceOne, resources.performanceTwo,
-        resources.performanceThree, resources.performanceFour
+        resources.performanceOne,
+        resources.performanceTwo,
+        resources.performanceThree,
+        resources.performanceFour
     ];
 
     constructor(private readonly game: Game) {
@@ -69,11 +83,25 @@ export class Performance extends Scene {
         vignette.addDrawing(resources.performanceVignette);
         this.add(vignette);
 
-        // TODO: tempoText, bpmText, scoreText, multiplierText, messageText
+    // TODO: tempoText, bpmText, scoreText, multiplierText, messageText
+        this.tempoText = new Label("897");
+        this.tempoText.color = new Color(0xee, 0xee, 0xee);
+        this.tempoText.fontSize = 36;
+    // tempoText..filters = [new GlowFilter(0x000000, 1, 4, 4)];
+        this.tempoText.x = 20;
+        this.tempoText.y = 40;
+        this.add(this.tempoText);
+        this.bpmText = new Label("Test");
+        this.bpmText.color = new Color(0xee, 0xee, 0xee);
+        this.bpmText.fontSize = 24;
+    // _bpmText.field.filters = [new GlowFilter(0x000000, 1, 3, 3)];
+        this.bpmText.x = 20;
+        this.bpmText.y = 75;
+        this.add(this.bpmText);
 
-        // TODO: stars
+    // TODO: stars
 
-        // TODO: instructionText
+    // TODO: instructionText
     }
 
     public onActivate(): void {
@@ -81,41 +109,44 @@ export class Performance extends Scene {
         this.add(this.game.music);
         this.game.music.stop();
 
-        // this.score = 0;
-        // this.multiplier = 0;
+    // this.score = 0;
+    // this.multiplier = 0;
         this.game.stars = 0;
         this.missedBeats = 0;
         this.introBeat = 0;
         this.timer.reset(this.game.bpm);
         this.arm.reset();
-        // TODO: set tempo text
-        // TODO: set bpm text
+    // TODO: set tempo text
+    // this.tempoText!.text = this.tempo;
+
+    // TODO: set bpm text
         this.beats = 16;
         this.state = State.intro;
         this.time = introDuration;
+        this.bpmText!.text = this.beats + "bpm";
 
-        // TODO: set alpha and scale for stars
+    // TODO: set alpha and scale for stars
 
-        // TODO: set message text
+    // TODO: set message text
 
-        // TODO: fade message
+    // TODO: fade message
 
-        resources.performanceReady.play()
-            .then(() => void 0,
-                reason => console.error("", reason));
+        resources.performanceReady.play().then(
+            () => void 0,
+            reason => console.error("", reason)
+    );
     }
-
 
     public update(engine: Engine, delta: number): void {
         super.update(engine, delta);
 
         this.timer.update(delta);
 
-        // TODO: Set multiplier text colour
+    // TODO: Set multiplier text colour
 
         switch (this.state) {
             case State.intro:
-                // Pause a bit before starting
+        // Pause a bit before starting
                 this.time -= delta;
                 if (this.time <= 0) {
                     this.transition(State.countIn);
@@ -123,9 +154,10 @@ export class Performance extends Scene {
                 break;
             case State.countIn:
                 if (this.timer.isBeat) {
-                    this.countdown[this.introBeat].play()
-                        .then(() => void 0,
-                            reason => console.error("", reason));
+                    this.countdown[this.introBeat].play().then(
+                        () => void 0,
+                        reason => console.error("", reason)
+          );
                     ++this.introBeat;
 
                     if (this.introBeat >= 4) {
@@ -141,60 +173,60 @@ export class Performance extends Scene {
                     --this.beats;
                 }
 
-                // TODO: should accept any key
+        // TODO: should accept any key
                 if (this.game.engine.input.keyboard.wasPressed(Input.Keys.Space)) {
-                    // TODO: Fade instructions
+          // TODO: Fade instructions
 
-                    const missed = this.timer.offBeatMs >= 4 / 60 * 1000;
+                    const missed = this.timer.offBeatMs >= (4 / 60) * 1000;
                     if (missed) {
                         this.arm.miss();
-                        // this.multiplier = 0;
-                        // TODO: Update and fade multiplier text
+            // this.multiplier = 0;
+            // TODO: Update and fade multiplier text
                     } else {
                         this.arm.tickTock();
-                        // ++this.multiplier;
-                        // TODO: Update multiplier text
+            // ++this.multiplier;
+            // TODO: Update multiplier text
                         this.missedBeats = 0;
                     }
 
-                    // What kind of hit did we register?
-                    if (this.timer.offBeatMs < 1 / 60 * 1000) {
-                        // TODO: Float "Perfect!"
-                        // this.multiplier += 4;
-                        // TODO: Update and fade multiplier text
-                        // TODO: Play chime
-                    } else if (this.timer.offBeatMs < 3 / 60 * 1000) {
-                        // TODO: Float "Good"
-                    } else if (this.timer.offBeatMs < 4 / 60 * 1000) {
-                        // Do nothing
-                    } else if (this.timer.offBeatMs < 7 / 60 * 1000) {
-                        // TODO: Float "Miss"
-                    } else if (this.timer.offBeatMs < 8 / 60 * 1000) {
-                        // Do nothing
-                        // TODO: Is this really intended?
-                    } else if (this.timer.offBeatMs < 10 / 60 * 1000) {
-                        // TODO: Float "Poor"
-                    } else {
-                        // TODO: Float "Awful!"
-                    }
+          // What kind of hit did we register?
+                    if (this.timer.offBeatMs < (1 / 60) * 1000) {
+            // TODO: Float "Perfect!"
+            // this.multiplier += 4;
+            // TODO: Update and fade multiplier text
+            // TODO: Play chime
+                    } else if (this.timer.offBeatMs < (3 / 60) * 1000) {
+            // TODO: Float "Good"
+          } else if (this.timer.offBeatMs < (4 / 60) * 1000) {
+            // Do nothing
+          } else if (this.timer.offBeatMs < (7 / 60) * 1000) {
+            // TODO: Float "Miss"
+          } else if (this.timer.offBeatMs < (8 / 60) * 1000) {
+            // Do nothing
+            // TODO: Is this really intended?
+          } else if (this.timer.offBeatMs < (10 / 60) * 1000) {
+            // TODO: Float "Poor"
+          } else {
+            // TODO: Float "Awful!"
+          }
 
-                    // Score some points if we hit.
+          // Score some points if we hit.
                     if (!missed) {
-                        // const factor = Math.max(0, 4 - Math.floor(this.timer.offBeatMs / 1000 * 60));
-                        // this.score += factor * this.multiplier * baseScore;
+            // const factor = Math.max(0, 4 - Math.floor(this.timer.offBeatMs / 1000 * 60));
+            // this.score += factor * this.multiplier * baseScore;
                     }
                 }
 
-                // Check to see if we should transition out of the play state.
+        // Check to see if we should transition out of the play state.
                 if (this.beats < 0 || this.missedBeats > maxMissedBeats) {
                     this.transition(State.outro);
                 }
                 break;
             case State.outro:
-                // TODO
+        // TODO
                 break;
             case State.done:
-                // TODO
+        // TODO
                 break;
         }
     }
@@ -205,24 +237,29 @@ export class Performance extends Scene {
         switch (state) {
             case State.countIn:
                 this.timer.start();
-                // TODO: show instructions
+        // TODO: show instructions
                 break;
             case State.play:
-                // TODO: fade message text
+        // TODO: fade message text
                 break;
             case State.outro:
-                // TODO: set message text
-                // TODO: display stars
+        // TODO: set message text
+        // TODO: display stars
 
                 this.time = outroDuration;
 
-                ((this.missedBeats <= maxMissedBeats) ? resources.performanceCheer : resources.performanceBoo)
+                (this.missedBeats <= maxMissedBeats
+          ? resources.performanceCheer
+          : resources.performanceBoo
+        )
                     .play()
-                    .then(() => void 0,
-                        reason => console.error("", reason));
+                    .then(
+              () => void 0,
+              reason => console.error("", reason)
+          );
                 break;
             case State.result:
-                // TODO
+        // TODO
                 break;
             case State.return:
                 this.game.engine.goToScene("menu");
