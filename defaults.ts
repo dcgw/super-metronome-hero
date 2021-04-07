348bb840eea1627006f912ab735348dc643a4ecc
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-04-07 02:11:52 +0100

    fix(resources): fix resource imports for compatibility with webpack-config v4

---
+++ defaults.ts
@@ -0,0 +1,13 @@
+import {LabelOptions} from "@dcgw/excalibur-extended-label";
+import {BaseAlign, Color} from "excalibur";
+
+export const defaultLabelOptions: LabelOptions = {
+    fontFamily: "Strait",
+    fontSize: 48,
+    baseAlign: BaseAlign.Top,
+    color: Color.White,
+    outlineColor: Color.fromRGB(0, 0, 0, 0.5),
+    outlineWidth: 1,
+    shadowColor: Color.Black,
+    shadowBlurRadius: 2
+};
