adf1911dc58e2ca6c3b75f0fb784fbc798610957
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-04-23 13:53:17 +0100

    build(webpack): delete commented code

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -16,36 +16,6 @@ module.exports = config({
             plugins: [
                 ...(configuration.resolve.plugins ?? []),
                 new ResolveTypescriptPlugin()
-                // {
-                //     apply: resolver => {
-                //         const target = resolver.ensureHook("file");
-                //         for (const extension of [".ts", ".tsx"]) {
-                //             resolver.getHook("raw-file").tapAsync("TestPlugin", (request, resolveContext, callback) => {
-                //                 if (!request.path || request.path.match(/(^|[\\/])node_modules($|[\\/])/)) {
-                //                     return callback();
-                //                 }
-                //
-                //                 const path = request.path.replace(/\.js$/, extension);
-                //                 if (path === request.path) {
-                //                     callback();
-                //                 } else {
-                //                     resolver.doResolve(
-                //                         target,
-                //                         {
-                //                             ...request,
-                //                             path,
-                //                             relativePath:
-                //                                 request.relativePath && request.relativePath.replace(/\.js$/, extension)
-                //                         },
-                //                         `using path: ${path}`,
-                //                         resolveContext,
-                //                         callback
-                //                     );
-                //                 }
-                //             });
-                //         }
-                //     }
-                // }
             ]
         }
     })
